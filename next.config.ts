817e0e0c53d73eba08587b554abaea47974aee23
greenyellowlight <greenyellowlight@protonmail.com>
2026-07-20 20:28:47 +1000

    path issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    
+    return [
+      {
+        source: '/garden-planner',
+        destination: `${process.env.GARDEN_PLANNER_DOMAIN}/garden-planner`,
+      },
+      {
+        source: '/garden-planner/:path+',
+        destination: `${process.env.GARDEN_PLANNER_DOMAIN}/garden-planner/:path+`,
+      },
+    ];
+  }
 };
 
 export default nextConfig;
