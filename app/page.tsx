5c086adff4e0b85e297ef1687d5744bab376f29c
greenyellowlight <greenyellowlight@protonmail.com>
2026-07-24 21:18:25 +1000

    layout improvements

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import PillLink from "./_components/PillLink";
+import Image from "next/image";
 
-const yearsOfExperience = ( () => {
+const yearsOfExperience = (() => {
   const startYear = 2024
   const startMonth = 6 // TODO: include various work beforehand?
 
   const now = new Date()
-  
+
   return now.getFullYear() - startYear + (startMonth > now.getMonth() ? 1 : 0)
 
 })();
@@ -14,22 +15,29 @@ const yearsOfExperience = ( () => {
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center bg-zinc-20 font-san">
-      <header className="w-full py-16 text-center">
-        <h1 className="text-6xl font-semibold tracking-tight text-black">
-          Linden Beaumont
-        </h1>
-        <p className="mx-auto mt-4 text-xl max-w-sm px-6 text-sm text-zinc-500 dark:text-zinc-400">
-          Melbourne based software developer with {yearsOfExperience} years experience. See my projects below or get in touch at{" "}
-          <a
+
+
+      <main className="flex flex-1 w-full max-w-xl flex-col items-center justify-center gap-4">
+        <Image 
+          src="/homepage-logo.svg"
+          alt="website logo"
+          width={300}
+          height={300}
+          className="size-32" 
+        />
+
+        <h1 className="text-6xl font-semibold tracking-tight text-center text-black">Linden Beaumont</h1>
+        <p className="mx-auto text-xl max-w-xl pt-2 pb-8 text-center text-sm text-zinc-500 dark:text-zinc-400">
+          Melbourne based software developer with {yearsOfExperience} years experience.
+          See my projects below or get in touch at{" "}
+        <a
             href="mailto:lindenbeaumont@protonmail.com"
-            className="text-black underline underline-offset-2 hover:text-zinc-600"
+            className="text-black underline underline-offset-2  text-zinc-700 hover:text-zinc-600"
           >
             lindenbeaumont@protonmail.com
           </a>
         </p>
-      </header>
 
-      <main className="flex flex-1 w-full max-w-md flex-col items-center justify-center gap-4 px-6">
         <div className="flex w-full flex-col items-center gap-2">
           <PillLink
             href="/garden-planner"
@@ -37,7 +45,7 @@ export default function Home() {
             iconSrc="/garden-planner.png"
             external={false}
           />
-          <p className="text-sm text-zinc-500 dark:text-zinc-400">Not on GitHub </p>
+          <p className="text-sm text-zinc-500 dark:text-zinc-400">(Not on GitHub)</p>
         </div>
 
         <PillLink
