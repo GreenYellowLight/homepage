80b949ef3b2b06c915ce2ca472fa1499303851e1
greenyellowlight <greenyellowlight@protonmail.com>
2026-08-17 12:20:00 +1000

    change page text

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,6 @@
 import PillLink from "./_components/PillLink";
 import Image from "next/image";
 
-const yearsOfExperience = (() => {
-  const startYear = 2024
-  const startMonth = 6 // TODO: include various work beforehand?
-
-  const now = new Date()
-
-  return now.getFullYear() - startYear + (startMonth > now.getMonth() ? 1 : 0)
-
-})();
-
-
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center bg-zinc-20 font-san">
@@ -28,14 +17,14 @@ export default function Home() {
 
         <h1 className="text-6xl font-semibold tracking-tight text-center text-black">Linden Beaumont</h1>
         <p className="mx-auto text-xl max-w-xl pt-1 pb-4 text-center text-sm text-zinc-500 dark:text-zinc-400">
-          Melbourne based software developer with {yearsOfExperience} years experience.
+          Melbourne based software developer.
           See my projects below or get in touch at{" "}
         <a
             href="mailto:lindenbeaumont@protonmail.com"
             className="text-black underline underline-offset-2  text-zinc-700 hover:text-zinc-600"
           >
             lindenbeaumont@protonmail.com
-          </a>
+          </a>.
         </p>
 
         <div className="flex w-full flex-col items-center gap-2">
@@ -45,7 +34,6 @@ export default function Home() {
             iconSrc="/garden-planner.svg"
             external={false}
           />
-          <p className="text-sm text-zinc-500 dark:text-zinc-400">(Not on GitHub)</p>
         </div>
 
         <PillLink
